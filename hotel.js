58149049bce5569c3f644de3be085d65f41dc319
Lukas-ctrlC-ctrlV <lukasjakubisin@gmail.com>
2025-05-13 10:17:42 +0200

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,23 @@
+let formHotel = document.querySelector(".hotel01")
+
+formHotel.addEventListener("submit",function(e){
+    e.preventDefault()
+
+    let hotelUser = {
+        jmeno: e.target.elements.Name.value,
+        prijmeni: e.target.elements.lastName.value,
+        narozeni: e.target.elements.birth.value,
+        mail: e.target.elements.mail.value,
+        heslo: e.target.elements.password.value,
+        pocetPokoju: e.target.elements.room.value
+    }
+
+console.log(
+`Jméno: ${hotelUser.jmeno}
+Příjmení: ${hotelUser.prijmeni}
+Narození: ${hotelUser.narozeni}
+Mail: ${hotelUser.mail}
+Heslo: ${hotelUser.heslo}
+Počet pokojů: ${hotelUser.pocetPokoju}`
+);
+})
